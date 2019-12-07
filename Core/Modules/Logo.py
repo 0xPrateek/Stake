03eaa8270546830f670da0aef6d1da123db7b4b6
Prateek Mishra <pr4t333k@gmail.com>
2019-12-07 20:43:57 +0530

    Stake Logo for Stake

---
+++ Core/Modules/Logo.py
@@ -0,0 +1,16 @@
+import Modules.colors as color
+
+def banner():
+
+    print('''
+
+{}    ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬
+{}    ▮       ▮           ▮          ▮         ▮           ▮      ▮
+{}    ▮    ▬ ▬ ▬ ▬     ▬ ▬ ▬ ▬     ▬ ▬ ▬     ▮    ▰      ▬ ▬ ▬    ▮
+{}    ▮   ▮               ▮       ▮     ▮    ▮  ▰       ▮         ▮
+{}    ▮    ▬ ▬ ▬ ▬        ▮       ▮ ▬ ▬ ▮    ▮▰         ▮ ▬ ▬     ▮
+{}    ▮           ▮       ▮       ▮     ▮    ▮  ▰       ▮         ▮
+{}    ▮    ▬ ▬ ▬ ▬        ▮       ▮     ▮    ▮    ▰      ▬ ▬ ▬    ▮
+{}    ▮       ▮           ▮          ▮         ▮           ▮      ▮
+{}    ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬
+{}'''.format(color.red,color.red,color.blue,color.blue,color.blue,color.blue,color.blue,color.red,color.red,color.end))
